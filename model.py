0ec8be43ebad9d0d3ab670a8ad29e3202286f2da
muheezokunade <muheezadedejiokunade@gmail.com>
2022-11-16 10:19:55 +0100

    Update model.py

--- model.py
+++ model.py
@@ -78,7 +78,7 @@ def _preprocess_data(data):
     
     # ------------------------------------------------------------------------
 
-    return predict_vector
+    return feature_vector_df
 
 def load_model(path_to_model:str):
     """Adapter function to load our pretrained model into memory.
