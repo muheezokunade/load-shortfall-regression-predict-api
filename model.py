0b3c49021d5b20361329f421b60ac267f1838bfd
muheezokunade <muheezadedejiokunade@gmail.com>
2022-11-16 10:17:54 +0100

    Update model.py

--- model.py
+++ model.py
@@ -75,18 +75,7 @@ def _preprocess_data(data):
     feature_vector_df['Hour_of_week'] = ((feature_vector_df['time'].astype('datetime64').dt.dayofweek) * 24 + 24) - (24 - df['time'].astype('datetime64').dt.hour)
     feature_vector_df['Hour_of_day']  = feature_vector_df['time'].astype('datetime64').dt.hour
 
-    feature_vector_df = feature_vector_df.drop(columns=['Week_of_year','Day_of_year','Hour_of_week','time'])
-    feature_vector_df = feature_vector_df[['Madrid_wind_speed', 'Valencia_wind_deg', 'Bilbao_rain_1h',
-       'Valencia_wind_speed', 'Seville_humidity', 'Madrid_humidity',
-       'Bilbao_clouds_all', 'Bilbao_wind_speed', 'Seville_clouds_all',
-       'Bilbao_wind_deg', 'Barcelona_wind_speed', 'Barcelona_wind_deg',
-       'Madrid_clouds_all', 'Seville_wind_speed', 'Barcelona_rain_1h',
-       'Seville_pressure', 'Seville_rain_1h', 'Bilbao_snow_3h',
-       'Barcelona_pressure', 'Seville_rain_3h', 'Madrid_rain_1h',
-       'Barcelona_rain_3h', 'Valencia_snow_3h', 'Madrid_weather_id',
-       'Barcelona_weather_id', 'Bilbao_pressure', 'Seville_weather_id',
-       'Valencia_pressure', 'Seville_temp_max', 'Bilbao_weather_id', 
-        'Valencia_humidity', 'Year', 'Month_of_year', 'Day_of_month', 'Day_of_week', 'Hour_of_day']]
+    
     # ------------------------------------------------------------------------
 
     return predict_vector
