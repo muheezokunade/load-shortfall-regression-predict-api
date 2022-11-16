a380893cc5e7bf67bdbbd75948d21388af326f2d
muheezokunade <muheezadedejiokunade@gmail.com>
2022-11-16 07:53:16 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,24 +1,18 @@
 """
-
     Helper functions for the pretrained model to be used within our API.
-
     Author: Explore Data Science Academy.
-
     Note:
     ---------------------------------------------------------------------
     Please follow the instructions provided within the README.md file
     located within this directory for guidance on how to use this script
     correctly.
-
     Importantly, you will need to modify this file by adding
     your own data preprocessing steps within the `_preprocess_data()`
     function.
     ----------------------------------------------------------------------
-
     Description: This file contains several functions used to abstract aspects
     of model interaction within the API. This includes loading a model from
     file, data preprocessing, and model prediction.  
-
 """
 
 # Helper Dependencies
@@ -29,16 +23,12 @@ import json
 
 def _preprocess_data(data):
     """Private helper function to preprocess data for model prediction.
-
     NB: If you have utilised feature engineering/selection in order to create
     your final model you will need to define the code here.
-
-
     Parameters
     ----------
     data : str
         The data payload received within POST requests sent to our API.
-
     Returns
     -------
     Pandas DataFrame : <class 'pandas.core.frame.DataFrame'>
@@ -48,37 +38,7 @@ def _preprocess_data(data):
     feature_vector_dict = json.loads(data)
     # Load the dictionary as a Pandas DataFrame.
     feature_vector_df = pd.DataFrame.from_dict([feature_vector_dict])
-
-    # ---------------------------------------------------------------
-    # NOTE: You will need to swap the lines below for your own data
-    # preprocessing methods.
-    # remove missing values/ features
-    # fill the missing value in Valencia_pressure with the mean, which is 1012.
-
-    #Train Data
-    #df.fillna(df.Valencia_pressure.mean(), inplace= True)
-
-    #Test data
-    #df_test.fillna(df_test.Valencia_pressure.mean(), inplace= True)
-
-    # Connvert both Seville_pressure to numerical columns by scrapping off the 'sp' infrom of it
-
-    #df.Seville_pressure = df.Seville_pressure.str[2:]
-
-    # Test data
-    #df_test.Seville_pressure = df_test.Seville_pressure.str[2:]
-    # Convert to data type int
-    #df['Seville_pressure'] = df['Seville_pressure'].astype(int)
-
-    # Test Data
-    #df_test['Seville_pressure'] = df_test['Seville_pressure'].astype(int)
-
-
-    # Convert the json string to a python dictionary object
-    feature_vector_dict = json.loads(data)
-    # Load the dictionary as a Pandas DataFrame.
-    feature_vector_df = pd.DataFrame.from_dict([feature_vector_dict])
-    new_f = [ 'Madrid_wind_speed', 'Bilbao_rain_1h',
+    new_f = ['Unnamed: 0', 'Madrid_wind_speed', 'Bilbao_rain_1h',
             'Valencia_wind_speed', 'Seville_humidity', 'Madrid_humidity',
             'Bilbao_clouds_all', 'Bilbao_wind_speed', 'Seville_clouds_all',
             'Bilbao_wind_deg', 'Barcelona_wind_speed', 'Barcelona_wind_deg',
@@ -93,28 +53,33 @@ def _preprocess_data(data):
             'Barcelona_temp', 'Bilbao_temp_min', 'Bilbao_temp',
             'Barcelona_temp_min', 'Bilbao_temp_max', 'Seville_temp_min',
             'Madrid_temp', 'Madrid_temp_min']
+
+    # ---------------------------------------------------------------
+    # NOTE: You will need to swap the lines below for your own data
+    # preprocessing methods.
     #
     # The code below is for demonstration purposes only. You will not
     # receive marks for submitting this code in an unchanged state.
     # ---------------------------------------------------------------
 
     # ----------- Replace this code with your own preprocessing steps --------
-    
+    predict_vector = feature_vector_df[new_f]
+    # ------------------------------------------------------------------------
+
+    return predict_vector
+
 def load_model(path_to_model:str):
     """Adapter function to load our pretrained model into memory.
-
     Parameters
     ----------
     path_to_model : str
         The relative path to the model weights/schema to load.
         Note that unless another file format is used, this needs to be a
         .pkl file.
-
     Returns
     -------
     <class: sklearn.estimator>
         The pretrained model loaded into memory.
-
     """
     return pickle.load(open(path_to_model, 'rb'))
 
@@ -125,19 +90,16 @@ def load_model(path_to_model:str):
 
 def make_prediction(data, model):
     """Prepare request data for model prediction.
-
     Parameters
     ----------
     data : str
         The data payload received within POST requests sent to our API.
     model : <class: sklearn.estimator>
         An sklearn model object.
-
     Returns
     -------
     list
         A 1-D python list containing the model prediction.
-
     """
     # Data preprocessing.
     prep_data = _preprocess_data(data)
