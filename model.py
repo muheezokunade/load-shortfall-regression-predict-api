1818c2f759fa1514392d563fa7c05da9eb018aff
muheezokunade <muheezadedejiokunade@gmail.com>
2022-11-21 11:39:56 +0100

    update

--- model.py
+++ model.py
@@ -1,18 +1,24 @@
 """
+
     Helper functions for the pretrained model to be used within our API.
+
     Author: Explore Data Science Academy.
+
     Note:
     ---------------------------------------------------------------------
     Please follow the instructions provided within the README.md file
     located within this directory for guidance on how to use this script
     correctly.
+
     Importantly, you will need to modify this file by adding
     your own data preprocessing steps within the `_preprocess_data()`
     function.
     ----------------------------------------------------------------------
+
     Description: This file contains several functions used to abstract aspects
     of model interaction within the API. This includes loading a model from
     file, data preprocessing, and model prediction.  
+
 """
 
 # Helper Dependencies
@@ -20,50 +26,31 @@ import numpy as np
 import pandas as pd
 import pickle
 import json
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
 
 def _preprocess_data(data):
     """Private helper function to preprocess data for model prediction.
+
     NB: If you have utilised feature engineering/selection in order to create
     your final model you will need to define the code here.
+
+
     Parameters
     ----------
     data : str
         The data payload received within POST requests sent to our API.
+
     Returns
     -------
     Pandas DataFrame : <class 'pandas.core.frame.DataFrame'>
-        The preprocessed data, ready to be used our model for prediction.
+        The preprocessed data, ready to be used by our model for prediction.
     """
     # Convert the json string to a python dictionary object
     feature_vector_dict = json.loads(data)
     # Load the dictionary as a Pandas DataFrame.
     feature_vector_df = pd.DataFrame.from_dict([feature_vector_dict])
-    feature_vector_df.fillna(0, inplace= True)
-    feature_vector_df.Seville_pressure = feature_vector_df.Seville_pressure.str[2:]
-    feature_vector_df['Seville_pressure'] = feature_vector_df['Seville_pressure'].astype(int)
-    feature_vector_df.Valencia_wind_deg = feature_vector_df.Valencia_wind_deg.str[6:]
-    feature_vector_df['Valencia_wind_deg'] = feature_vector_df['Valencia_wind_deg'].astype(int)
-    
-    feature_vector_df['Year']  = feature_vector_df['time'].astype('datetime64').dt.year
-    feature_vector_df['Month_of_year']  = feature_vector_df['time'].astype('datetime64').dt.month
-    feature_vector_df['Week_of_year'] = feature_vector_df['time'].astype('datetime64').dt.weekofyear
-    feature_vector_df['Day_of_year']  = feature_vector_df['time'].astype('datetime64').dt.dayofyear
-    feature_vector_df['Day_of_month']  = feature_vector_df['time'].astype('datetime64').dt.day
-    feature_vector_df['Day_of_week'] = feature_vector_df['time'].astype('datetime64').dt.dayofweek
-    feature_vector_df['Hour_of_week'] = ((feature_vector_df['time'].astype('datetime64').dt.dayofweek) * 24 + 24) - (24 - df['time'].astype('datetime64').dt.hour)
-    feature_vector_df['Hour_of_day']  = feature_vector_df['time'].astype('datetime64').dt.hour
-    feature_vector_df = feature_vector_df.drop(columns=['Week_of_year','Day_of_year','Hour_of_week','time'])
-    feature_vector_df = feature_vector_df[['Madrid_wind_speed', 'Valencia_wind_deg', 'Bilbao_rain_1h',
-       'Valencia_wind_speed', 'Seville_humidity', 'Madrid_humidity',
-       'Bilbao_clouds_all', 'Bilbao_wind_speed', 'Seville_clouds_all',
-       'Bilbao_wind_deg', 'Barcelona_wind_speed', 'Barcelona_wind_deg',
-       'Madrid_clouds_all', 'Seville_wind_speed', 'Barcelona_rain_1h',
-       'Seville_pressure', 'Seville_rain_1h', 'Bilbao_snow_3h',
-       'Barcelona_pressure', 'Seville_rain_3h', 'Madrid_rain_1h',
-       'Barcelona_rain_3h', 'Valencia_snow_3h', 'Madrid_weather_id',
-       'Barcelona_weather_id', 'Bilbao_pressure', 'Seville_weather_id',
-       'Valencia_pressure', 'Seville_temp_max', 'Bilbao_weather_id', 
-        'Valencia_humidity', 'Year', 'Month_of_year', 'Day_of_month', 'Day_of_week', 'Hour_of_day']]
 
     # ---------------------------------------------------------------
     # NOTE: You will need to swap the lines below for your own data
@@ -74,43 +61,60 @@ def _preprocess_data(data):
     # ---------------------------------------------------------------
 
     # ----------- Replace this code with your own preprocessing steps --------
-    predict_vector = feature_vector_df
+    df_train = feature_vector_df.copy()
+    df_train['time'] = pd.to_datetime(df_train['time'])
+    df_train['year'] = df_train['time'].dt.year
+    df_train['month'] = df_train['time'].dt.month
+    df_train['day'] = df_train['time'].dt.day
+    df_train['hour'] = df_train['time'].dt.hour
+    for col in df_train.columns:
+        if True in list(df_train[col].isna()):
+            df_train = df_train.dropna(subset=col)
+    df_train.Valencia_wind_deg = df_train.Valencia_wind_deg.str[6:]
+    df_train['Valencia_wind_deg'] = df_train['Valencia_wind_deg'].astype(int)
+    df_train['Seville_pressure'] = df_train['Seville_pressure'].str[2:]
+    df_train['Seville_pressure'] = df_train['Seville_pressure'].astype(int)
+    predict_vector = df_train.drop(['load_shortfall_3h','Unnamed: 0', 'time'], axis = 1)
     # ------------------------------------------------------------------------
-
     return predict_vector
 
-def load_model(path_to_model:str):
+def load_model(path_to_model):
     """Adapter function to load our pretrained model into memory.
+
     Parameters
     ----------
     path_to_model : str
         The relative path to the model weights/schema to load.
         Note that unless another file format is used, this needs to be a
         .pkl file.
+
     Returns
     -------
     <class: sklearn.estimator>
         The pretrained model loaded into memory.
+
     """
     return pickle.load(open(path_to_model, 'rb'))
-
-
+    
+model = load_model('teamND3_model.pkl')
 """ You may use this section (above the make_prediction function) of the python script to implement 
     any auxiliary functions required to process your model's artifacts.
 """
-
 def make_prediction(data, model):
     """Prepare request data for model prediction.
+
     Parameters
     ----------
     data : str
         The data payload received within POST requests sent to our API.
     model : <class: sklearn.estimator>
         An sklearn model object.
+
     Returns
     -------
     list
         A 1-D python list containing the model prediction.
+
     """
     # Data preprocessing.
     prep_data = _preprocess_data(data)
@@ -118,3 +122,5 @@ def make_prediction(data, model):
     prediction = model.predict(prep_data)
     # Format as list for output standardisation.
     return prediction[0].tolist()
+
+
